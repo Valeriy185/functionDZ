a7ca7e0aa1d337e068c9f8baace49d9a234e0ce4
Валерий <102753796+Valeriy185@users.noreply.github.com>
2022-11-09 23:18:41 +0300

    encoding

--- FunctionDZ/main.cpp
+++ FunctionDZ/main.cpp
@@ -1,29 +1,29 @@
-#include<iostream>
+п»ї#include<iostream>
 #include<math.h>
 #define DEGREE
 using namespace std;
-//int Power(int a, int n); //Прототип функции
+//int Power(int a, int n); //РџСЂРѕС‚РѕС‚РёРї С„СѓРЅРєС†РёРё
 void main()
 {
 	setlocale(LC_ALL, "");
 #ifdef DEGREE
 	int N, a, n;
-	cout << "Введите число: "; cin >> a; cout << endl;
-	cout << "Введите степень числа: "; cin >> n; cout << endl;
+	cout << "Р’РІРµРґРёС‚Рµ С‡РёСЃР»Рѕ: "; cin >> a; cout << endl;
+	cout << "Р’РІРµРґРёС‚Рµ СЃС‚РµРїРµРЅСЊ С‡РёСЃР»Р°: "; cin >> n; cout << endl;
 	N = pow(a, n);
 	cout << N;
 #endif // DEGREE
 
      //int a, n;
-	//cout << "Введите число: "; cin >> a; cout << endl;
-	//cout << "Введите степень числа: "; cin >> n; cout << endl;
-	//int N = Power(a, n); //Вызов функции
-	//cout << a << " В степени " << n << " = " << Power(a, n) << endl;
+	//cout << "Р’РІРµРґРёС‚Рµ С‡РёСЃР»Рѕ: "; cin >> a; cout << endl;
+	//cout << "Р’РІРµРґРёС‚Рµ СЃС‚РµРїРµРЅСЊ С‡РёСЃР»Р°: "; cin >> n; cout << endl;
+	//int N = Power(a, n); //Р’С‹Р·РѕРІ С„СѓРЅРєС†РёРё
+	//cout << a << " Р’ СЃС‚РµРїРµРЅРё " << n << " = " << Power(a, n) << endl;
 
 }
-/*int Power(int a, int n) //Реализация функции
+/*int Power(int a, int n) //Р РµР°Р»РёР·Р°С†РёСЏ С„СѓРЅРєС†РёРё
 {
-	int  N = a ^ n; //НЕ РАБОТАЕТ :-(((  !!!
+	int  N = a ^ n; //РќР• Р РђР‘РћРўРђР•Рў :-(((  !!!
 	return N;
 
 	
